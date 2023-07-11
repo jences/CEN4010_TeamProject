e5a7f79570a265607ba237d827a1c7f7fef66cc5
J. E. Cespedes <119894549+jences@users.noreply.github.com>
2023-07-11 05:09:00 -0400

    updated classes in shoppingcart/models

--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-from bookstore_app.models import Book, Author, Publisher, WebsiteUser
+from bookstore_app.models import Book
 
-class OrderItem(models.Model):
+class CartItem(models.Model):
     product = models.OneToOneField(Book, on_delete=models.SET_NULL, null=True)
     is_ordered = models.BooleanField(default=False)
 
     def __str__(self):
         return self.product.name
     
-class Order(models.Model):
+class Cart(models.Model):
     ref_code=models.CharField(max_length=15)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     is_ordered = models.BooleanField(default=False)

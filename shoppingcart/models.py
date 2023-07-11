499b66cf502b02de5781fd2ecc2d152dbb8ce1fc
J. E. Cespedes <119894549+jences@users.noreply.github.com>
2023-07-11 05:03:24 -0400

    updated shoppingcart/models

--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -11,4 +11,16 @@ class OrderItem(models.Model):
     
 class Order(models.Model):
     ref_code=models.CharField(max_length=15)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    is_ordered = models.BooleanField(default=False)
+
+    def get_cart_items(self):
+        return self.items.all()
+    
+    def get_cart_total(self):
+        return sum([item.product.price for item in self.items.all()])
+    
+    def __str__(self):
+        return '{0} - {1}'.format(self.owner, self.ref_code)
+    
 # Create your models here.

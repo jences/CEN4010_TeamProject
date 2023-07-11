e2afb0d8b37735318d900327b63cb4d2855a177f
J. E. Cespedes <119894549+jences@users.noreply.github.com>
2023-07-11 05:54:27 -0400

    updated imports

--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -5,14 +5,17 @@ from bookstore_app.models import Book
 class CartItem(models.Model):
     product = models.OneToOneField(Book, on_delete=models.SET_NULL, null=True)
     is_ordered = models.BooleanField(default=False)
+    quantity = models.IntegerField(default=0)
+    price_ht = models.FloatField(blank=True)
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.product.name
     
 class Cart(models.Model):
-    ref_code=models.CharField(max_length=15)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    is_ordered = models.BooleanField(default=False)
+    #ref_code=models.CharField(max_length=15)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    #is_ordered = models.BooleanField(default=False)
 
     def get_cart_items(self):
         return self.items.all()

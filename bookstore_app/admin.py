7946253f38189b438d4ef27eecb63b9e41e50ea7
J. E. Cespedes <119894549+jences@users.noreply.github.com>
2023-07-11 21:00:12 -0400

    added CartItem to admin

--- bookstore_app/admin.py
+++ bookstore_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Book, Author, Publisher, WebsiteUser, Cart
+from .models import Book, Author, Publisher, WebsiteUser, Cart, CartItem
 
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Publisher)
 admin.site.register(WebsiteUser)
 admin.site.register(Cart)
+admin.site.register(CartItem)

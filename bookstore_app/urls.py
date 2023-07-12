04d84414131dae49690d20fc90cf0b2603457025
J. E. Cespedes <119894549+jences@users.noreply.github.com>
2023-07-11 21:15:11 -0400

    corrected urls

--- bookstore_app/urls.py
+++ bookstore_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from bookstore_app import views
-from .views import CartItemViews
+from .views import CartViews
 
 urlpatterns = [
     path('register/', views.RegisterAPI.as_view(), name="register"),
     path('login/', views.LoginAPI.as_view(), name="login"),
     path('profile/', views.WebsiteUserAPI.as_view(), name='profile'),
     path('logout/', views.LogoutAPI.as_view(), name='logout'),
-    path('cart-items/', CartItemViews.as_view()),
+    path('cart-items/', CartViews.as_view()),
 ]
